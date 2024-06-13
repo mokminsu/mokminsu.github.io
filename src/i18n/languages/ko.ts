87a2483d713366e9ae7fe83930886c14d7e2f144
Mok Minsu <mokminsu7@gmail.com>
2024-06-13 21:02:22 +0900

    Feat: Add Ko i18n

---
+++ src/i18n/languages/ko.ts
@@ -0,0 +1,37 @@
+import Key from '../i18nKey'
+import type { Translation } from '../translation'
+
+export const ko: Translation = {
+  [Key.home]: '홈',
+  [Key.about]: '소개',
+  [Key.archive]: '아카이브',
+
+  [Key.tags]: '태그',
+  [Key.categories]: '카테고리',
+  [Key.recentPosts]: '최근 게시물',
+
+  [Key.comments]: '댓글',
+
+  [Key.untitled]: '제목 없음',
+  [Key.uncategorized]: '미분류',
+  [Key.noTags]: '태그 없음',
+
+  [Key.wordCount]: '단어',
+  [Key.wordsCount]: '단어들',
+  [Key.minuteCount]: '분',
+  [Key.minutesCount]: '분들',
+  [Key.postCount]: '게시물',
+  [Key.postsCount]: '게시물들',
+
+  [Key.themeColor]: '테마 색상',
+
+  [Key.lightMode]: '라이트',
+  [Key.darkMode]: '다크',
+  [Key.systemMode]: '시스템',
+
+  [Key.more]: '더 보기',
+
+  [Key.author]: '작성자',
+  [Key.publishedAt]: '게시일',
+  [Key.license]: '라이선스',
+}
